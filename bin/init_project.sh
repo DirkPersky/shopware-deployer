7742523ee0df5f112564daac57b1bcd324980fa1
Dirk.Persky <dirk.persky@gmail.com>
2022-03-03 15:25:23 +0100

    deploy init

--- bin/init_project.sh
+++ bin/init_project.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
 
 green=`tput setaf 2`
+yellow=`tput setaf 3`
 reset=`tput sgr0`
 
 #Project GIT
-read -p "Geben sie den Namen des neuen .GIT an:" gitname
+read -p "${yellow}?${reset} Geben sie den Namen des neuen .GIT an: " gitname
 echo -e "${green}\u2714${reset} Projekt wird vorbereitet"
-git remote add upstream git@github.com:shopware/production.git
-
-
-# get Shopware
-git clone git@github.com:shopware/production.git $name
-
-echo -e "${green}\u2714${reset} .GIT-Repo wird vorbereitet"
-# switch directory and remove hisory
-cd $name
+echo -e "${green}\u2714${reset} .GIT-Repos werden verknüpft"
 rm -rf .git
-# init new .git
 git init
 git add --all
+git remote add origin $gitname
+git remote add upstream git@github.com:shopware/production.git
+echo -e "${green}\u2714${reset} check Shopware"
+git fetch upstream
+read -p "${yellow}?${reset} Welche Shopware Version soll verwendet werden?: " gitbranch
+echo -e "${green}\u2714${reset} Merge Shopware Version"
+git merge -X theirs upstream/$gitbranch --allow-unrelated-histories
+echo -e "${green}\u2714${reset} Merge abgeschlossen!"
+git add --all
+echo -e "${green}\u2714${reset} Commit .git"
 git commit -m "Project Start"
 git branch -M main
-
 echo -e "${green}\u2714${reset} upload zum .GIT-Repo"
 #add new origin
-git remote add origin $gitname
 git push -u origin main --force
-# add remote upstream
-git remote add upstream git@github.com:shopware/production.git
-
 echo -e "${green}\u2714${reset} Projekt angelegt"
 
-#echo -e "${green}\u2714${reset} Prepare Remote Server"
-#ssh-keygen -t rsa -b 4096 -f $name
-#ssh-copy-id -i $name.pub p593040@p593040.mittwaldserver.info
-#rm $name.pub
+echo -e "${green}---------------------------------${reset}"
+echo -e "${green}\u2714${reset} Prepare Remote Server"
+read -p "${yellow}?${reset} SSH USER: " sshusername
+read -p "${yellow}?${reset} SS HOST: " sshhost
+mkdir -p var/ssh/
+ssh-keygen -t rsa -b 4096 -f var/ssh/ssh_remote
+ssh-copy-id -i var/ssh/ssh_remote.pub $sshusername@$sshhost
+rm var/ssh/ssh_remote.pub
+
+echo -e "${green}\u2714${reset} SSH verbunden"
+echo -e "${green}---------------------------------${reset}"
+echo -e "${yellow}!!! Bitte legen Sie SECRETS GIT an !!!${reset}"
+echo -e "${yellow}!!! DEPLOYMENT_SERVER:  ${sshhost}!!!${reset}"
+echo -e "${yellow}!!! SSH_PRIVATE_KEY:  cat >> ssh_remote!!!${reset}"
+
+
